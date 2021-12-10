cc210925ccf777596b1854e3479a04df237ef4c5
Bglaws <glaws.brian@gmail.com>
2021-12-09 21:56:53 -0500

    day 3 problem 2 progress

--- day3/problem1.py
+++ day3/problem1.py
@@ -21,7 +21,57 @@ def get_gamma_epsilon(lines):
 
 	return gamma * epsilon
 
+def oxygen_generator_rating(lines):
+	count0 = 0
+	count1 = 0
+	i = 0
 
-if __name__ == "__main__":
-	print(get_gamma_epsilon(read_input("./input.txt")))
+	while len(lines) > 1 and i < len(lines):
+		count0, count1 = sum_of_column(lines, i) 
+		if count0 > count1:
+			lines = bit_filter(lines, i, "0")
+		else:
+			lines = bit_filter(lines, i, "1")
+		i += 1
+
+	return lines[0]
+
+
+def CO2_generator_rating(lines):
+	count0 = 0
+	count1 = 0
+	i = 0
+
+	while len(lines) > 1 and i < len(lines):
+		count0, count1 = sum_of_column(lines, i) 
+		if count0 <= count1:
+			lines = bit_filter(lines, i, "0")
+		else:
+			lines = bit_filter(lines, i, "1")
+
+		i += 1
 	
+	return lines[0]
+
+			 		
+def sum_of_column(lines, col): 
+	pass
+	count = 0
+	for row in range(len(lines)):
+		count += int(lines[row][col])
+	return len(lines) - count, count
+
+def bit_filter(lines, col, bit):
+	ls = [] 
+	for num in lines:
+		if num[col] == bit:
+			ls.append(num)
+	return ls
+			
+if __name__ == "__main__":
+	path = "./input.txt"
+	path2 = "./test.txt"
+	lines = read_input(path)
+	lines2 = read_input(path2)
+	print(get_gamma_epsilon(read_input(path)))
+	print(int(oxygen_generator_rating(lines2), 2) * int(CO2_generator_rating(lines2), 2))	
